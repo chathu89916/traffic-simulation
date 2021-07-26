41be08f05304647ed0af4cf82aa98f30bcb575fc
chathu89916 <chathuracha@gmail.com>
2021-07-26 20:08:10 +0530

    added tensorflow

--- sketch.js
+++ sketch.js
@@ -11,8 +11,6 @@ function setup() {
   frameRate(60);
   initialPos = createVector(width/2, height/2);
   noStroke();
-  //Cars = new Array<addCar>(addCar);
-  //console.log(initialPos);
   Cars.push(new addCar(random(direction),random(turnDirection)));
   TrafficLights = new junction();
   setInterval(function(){TrafficLights.counter1();
@@ -34,24 +32,16 @@ function draw() {
   TrafficLights.lightUp();
   TrafficLights.lightLeft();
   TrafficLights.lightRight();
-  //TrafficLights.redLightDown();
-  //console.log(TrafficLights.redLight());
-  //console.log(Cars.length);
 
   for (var i = Cars.length - 1; i >= 0; i--) {
     Cars[i].display();
     Cars[i].follow();
     Cars[i].lightCheck(lightDirection, Cars);
-    //Cars[i].hit(Cars);
-    setInterval(Cars[i].queuCars(Cars), 1000);
-    //Cars[i].queuCars(Cars);
+    //setInterval(Cars[i].queuCars(Cars), 1000);
     Cars[i].update();
-    //Cars[i].signalcheck(signal);
-    
-    //Cars[i].turning(Cars[Cars.length-1].pos);
+
     
     if (Math.abs(initialPos.dist(Cars[Cars.length - 1].pos)) < random(width/2 - 300, width/2-50) && Cars.length < numberofCars) {
-      //console.log(i, initialPos.dist(Cars[i].pos), 'car added');
       Cars.push(new addCar(random(direction),random(turnDirection)));
     }
     if (Cars[i].pos.dist(initialPos) > 310) {
