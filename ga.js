9c62104bae077e03effdfa629c7dbb8bab4c6ad1
chathu89916 <chathuracha@gmail.com>
2021-08-03 00:34:53 +0530

    new best

--- ga.js
+++ ga.js
@@ -1,22 +1,29 @@
 function nextGeneration() {
   console.log("Next Generation");
   calculateFitness();
-  if (
-    saveTrafficLight == null ||
-    saveTrafficLight.fitness < TrafficLight.fitness
-  ) {
+  if (typeof saveTrafficLight == "undefined") {
+    console.log("no brain");
+    saveTrafficLight = new junction(TrafficLight.brain);
+  } else if (saveTrafficLight.fitness < TrafficLight.fitness) {
     console.log("high fitness");
+    saveTrafficLight.dispose();
     saveTrafficLight = new junction(TrafficLight.brain);
     saveTrafficLight.fitness = TrafficLight.fitness;
-    TrafficLight.mutate(0.05);
+    TrafficLight.mutate(0.1);
   } else if (saveTrafficLight.fitness >= TrafficLight.fitness) {
     TrafficLight.dispose();
-    saveTrafficLight.mutate(0.05);
+    //saveTrafficLight.mutate(0.05);
     TrafficLight = new junction(saveTrafficLight.brain);
+    TrafficLight.mutate(0.1);
   }
 }
 
 function calculateFitness() {
-  TrafficLight.fitness = carLeave / frameCount;
+  let remain = 0;
+  for (let i = 0; i < carCount.length; i++) {
+    remain += carCount[i];
+  }
+  //console.log(remain);
+  TrafficLight.fitness = (carLeave - remain) / frameCount;
   console.log(TrafficLight.fitness);
 }
